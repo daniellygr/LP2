58e2603d51895da0346508aacc89c1e30778bf96
Danielly Gallão Rocha <112280193+daniellygr@users.noreply.github.com>
2023-05-25 14:56:56 -0300

    Add files via upload

---
+++ Atividade9/PMatriz/PMatriz/Exercicio6.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace PMatriz
+{
+    partial class Exercicio6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.lstBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Location = new System.Drawing.Point(76, 54);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(75, 23);
+            this.btnExecutar.TabIndex = 0;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // lstBox
+            // 
+            this.lstBox.FormattingEnabled = true;
+            this.lstBox.Location = new System.Drawing.Point(190, 54);
+            this.lstBox.Name = "lstBox";
+            this.lstBox.Size = new System.Drawing.Size(240, 186);
+            this.lstBox.TabIndex = 1;
+            // 
+            // Exercicio6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstBox);
+            this.Controls.Add(this.btnExecutar);
+            this.Name = "Exercicio6";
+            this.Text = "Exercicio6";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.ListBox lstBox;
+    }
+}
---
+++ Atividade9/PMatriz/PMatriz/Exercicio6.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PMatriz
+{
+    public partial class Exercicio6 : Form
+    {
+        public Exercicio6()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            string[] nomes = new string[7];
+
+            for (int i = 0; i < 7; i++)
+            {
+                nomes[i] = Interaction.InputBox("Digite o nome " + (i + 1) + ":", "Entrada: ");
+            }
+
+            lstBox.Items.Clear();
+            foreach (var item in nomes)
+            {
+                var nomeSemEspaco = item.Replace(" ", "");
+                lstBox.Items.Add($"O nome: {item} tem {nomeSemEspaco.Length} caracteres!");
+            }
+        }
+    }
+}
