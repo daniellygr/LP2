5f60f7d1f92cc70a7aae1ac0cda42be2eaae51e5
Danielly Gallão Rocha <112280193+daniellygr@users.noreply.github.com>
2023-05-26 20:10:36 -0300

    Add files via upload

---
+++ Prova/PCotacao/PCotacao/Prova.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace PCotacao
+{
+    partial class Prova
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.lstbxCotacao = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Location = new System.Drawing.Point(19, 25);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(75, 23);
+            this.btnExecutar.TabIndex = 0;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.BtnExecutar_Click);
+            // 
+            // lstbxCotacao
+            // 
+            this.lstbxCotacao.FormattingEnabled = true;
+            this.lstbxCotacao.Location = new System.Drawing.Point(116, 25);
+            this.lstbxCotacao.Name = "lstbxCotacao";
+            this.lstbxCotacao.Size = new System.Drawing.Size(532, 277);
+            this.lstbxCotacao.TabIndex = 1;
+            // 
+            // Prova
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 367);
+            this.Controls.Add(this.lstbxCotacao);
+            this.Controls.Add(this.btnExecutar);
+            this.Name = "Prova";
+            this.Text = "Prova LP2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.ListBox lstbxCotacao;
+    }
+}
+
---
+++ Prova/PCotacao/PCotacao/Prova.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace PCotacao
+{
+    public partial class Prova : Form
+    {
+        public Prova()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnExecutar_Click(object sender, EventArgs e)
+        {
+            double[,] cotacao = new double[9,3];
+            double[] mediaGeral = new double[9];
+            double soma = 0;
+            double auxiliar;
+            double media;
+            string recebimento;
+
+            for (int i = 0; i < 9; i++) //Modelo de notebook
+            {
+                for (int j = 0; j < 3; j++) //Lojas
+                {
+                    //Entrada de dados do usuário
+                    recebimento = Interaction.InputBox($"Digite o valor do modelo {i+1} da loja {j + 1}:", "Entrada de dados");
+
+                    // Verificar se o usuário cancelou a entrada de dados
+                    if (string.IsNullOrEmpty(recebimento))
+                    {
+                        MessageBox.Show("Entrada de dados cancelada. O programa será encerrado.");
+                        return; // Encerrar o programa
+                    }
+                    //Verificar se a entrada é um valor numérico
+                    else if (!double.TryParse(recebimento, out double nota))
+                    {
+                        MessageBox.Show("Digite um valor válido!");
+                        j--;
+                    }
+                    //Verificar se o valor é maior que zero
+                    else if (nota <= 0)
+                    {
+                        MessageBox.Show("Digite um valor maior que zero");
+                        j--;
+                    }
+                    //Atribuir o valor recebido na matriz
+                    else
+                    {
+                        cotacao[i, j] = nota;
+                    }
+                }
+
+                //Cálculo da média de cada notebook nas 3 lojas
+                media = ((cotacao[i, 0]) + (cotacao[i, 1]) + (cotacao[i, 2])) / 3;
+
+                //Adicionar a informação de cada notebook no Listbox
+                lstbxCotacao.Items.Add($"Notebook {i + 1} - Loja 1: {(cotacao[i, 0]):C2}, loja 2: {(cotacao[i, 1]):C2} e loja 3: {(cotacao[i, 2]):C2}. Média: {media:C2}");
+
+                //Atribuir a media de cada notebook ao vetor de média geral
+                mediaGeral[i] = media;
+            }
+
+            lstbxCotacao.Items.Add("-----------------------------------------------------");
+
+            //Incrementar a media unitária na variável 'soma'
+            for (int i = 0; i < 9; i++)
+            {
+                soma += mediaGeral[i];
+            }
+
+            //Cálculo da média geral
+            auxiliar = soma / 9;
+
+            //Adicionar a informação da média geral ao Listbox
+            lstbxCotacao.Items.Add($"Média geral dos computadores: {auxiliar:C2}");
+
+        }
+    }
+}
